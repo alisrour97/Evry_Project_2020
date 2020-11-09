7705048de30bc6ef3902ab9b8c52063b61a2d1af
= <=>
2020-11-09 16:46:16 +0100

    Centralized plugin + environment + new flag models

--- evry_project_plugins/src/distance_to_flag_plugin.cpp
+++ evry_project_plugins/src/distance_to_flag_plugin.cpp
@@ -1,7 +1,7 @@
 #include <evry_project_plugins/distance_to_flag_plugin.hpp>
 
 namespace gazebo{
-  DistanceToFlagPlugin::DistanceToFlagPlugin(): _distribution(0, 0.1){
+  DistanceToFlagPlugin::DistanceToFlagPlugin(): _distribution(0, 0.05){
   }
 
   DistanceToFlagPlugin::~DistanceToFlagPlugin(){
@@ -9,22 +9,39 @@ namespace gazebo{
     delete _nh;
   }
 
-  void DistanceToFlagPlugin::Load(physics::ModelPtr model, sdf::ElementPtr sdf){
-    int id = sdf->GetElement("id")->Get<int>();
+  void DistanceToFlagPlugin::Load(physics::WorldPtr world, sdf::ElementPtr sdf){
+    _nbFlags = sdf->GetElement("nbFlags")->Get<int>();
 
-    _link = model->GetLink();
+    //get the flags poses and store it in the list _flags_poses
+    for(int i = 1; i < _nbFlags+1; i++){
+      ignition::math::Pose3d model_pose = world->ModelByName("flag_" + std::to_string(i))->WorldPose();
+      geometry_msgs::Pose2D flag_pose;
+      flag_pose.x = model_pose.Pos().X();
+      flag_pose.y = model_pose.Pos().Y();
+      _flags_poses.push_back(flag_pose);
+    }
 
     _nh = new ros::NodeHandle();
-    _dtf_service = _nh->advertiseService("dist_to_flag_number_" + std::to_string(id), &DistanceToFlagPlugin::distanceToFlag, this);
+    _dtf_service = _nh->advertiseService("distanceToFlag", &DistanceToFlagPlugin::distanceToFlag, this);
+  }
+
+  float DistanceToFlagPlugin::getMinDist(const geometry_msgs::Pose2D& msg){
+    float min_dist = FLT_MAX;
+    float dx, dy, dist;
+    for(geometry_msgs::Pose2D pose: _flags_poses){
+      dx = pose.x - msg.x;
+      dy = pose.y - msg.y;
+      dist = std::sqrt(dx*dx + dy*dy);
+      if(dist < min_dist)
+        min_dist = dist;
+    }
+    return min_dist;
   }
 
   bool DistanceToFlagPlugin::distanceToFlag(evry_project_plugins::DistanceToFlag::Request& req, evry_project_plugins::DistanceToFlag::Response& res){
-    ignition::math::Pose3d flag_pose = _link->WorldPose();
-    float dx = flag_pose.Pos().X() - req.agent_pose.x;
-    float dy = flag_pose.Pos().Y() - req.agent_pose.y;
-    res.distance = std::sqrt(dx*dx + dy*dy) + _distribution(_generator);
+    res.distance = getMinDist(req.agent_pose) + _distribution(_generator);
     return true;
   }
 
-  GZ_REGISTER_MODEL_PLUGIN(DistanceToFlagPlugin)
+  GZ_REGISTER_WORLD_PLUGIN(DistanceToFlagPlugin)
 }
